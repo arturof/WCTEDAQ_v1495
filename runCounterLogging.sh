7e34c000746c76dee0656d7df4a3289578dc6e28
Arturo Fiorentini <arturo.fiorentini@cunet.carleton.ca>
2025-03-24 15:24:39 +0100

    added json configuration file as input parameter to v1495-counters-database

--- runCounterLogging.sh
+++ runCounterLogging.sh
@@ -3,4 +3,5 @@
 cd /home/mpmt/Monitoring/libDAQInterface
 source Setup.sh
 cd /home/mpmt/firmware/v1495
-./v1495-counters-database -l a4818 -a 25501 -c 0 -v 3210 -r >& /dev/null &
+json_config=/home/mpmt/firmware/TriggerConfig/configurations/written_config.json
+./v1495-counters-database -l a4818 -a 25501 -c 0 -v 3210 -j ${json_config} -r >& /dev/null &
